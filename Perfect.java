794d3ef9df051eb619a27cf03136e36d0e31e0b4
Tomer Yamin <tomery@vintage-ip.com>
2024-01-26 14:18:39 +0200

    HW2

--- Perfect.java
+++ Perfect.java
@@ -4,18 +4,22 @@
 public class Perfect {
 	public static void main (String[] args) {
 	int num1 = Integer.parseInt(args[0]);
-	int divisor = 1;
-	int sumDivisors = 0;
+	int divisor = 2;
+	int sumDivisors = 1;
+	String finalText = num1 + " is a perfect number since " + num1 + " = 1";
+
 	while (divisor < num1) {
 		if (num1%divisor == 0) {
 		sumDivisors += divisor;
+		finalText = finalText + " + " + divisor;
 		}
+			  
 	divisor++;
 
 	}
 
 	if (num1 == sumDivisors) {
-	System.out.println(num1 + " is a perfect number");	
+	System.out.println(finalText);	
 
 	} 
 	
