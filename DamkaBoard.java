67cb8e3cdb6c06038b4c150a80f9487fffcd3989
Tomer Yamin <tomery@vintage-ip.com>
2024-01-26 14:27:42 +0200

    HW2

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -4,15 +4,20 @@
 public class DamkaBoard {
 	public static void main(String[] args) {
 		int num1 = Integer.parseInt(args[0]);
+
 	for (int i = 0; i < num1; i++) {
-		if (i%2 != 0) {
-		System.out.print(" ");
-		}
 		for (int j = 0; j < num1; j++) {
-		System.out.print("* ");
+			if (i%2 == 0) {
+				System.out.print("* ");
+			}
+			
+			else {
+				System.out.print(" *");
+			}	
 		}
-	System.out.println();
+	System.out.println();		
 	}
 
-	}
+
+	} 
 }
